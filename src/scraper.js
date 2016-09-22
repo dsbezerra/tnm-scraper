2ebfbc34c11ca63f93d18ba163a2164aca18c14d
Diego Bezerra <Diego Bezerra>
2016-09-22 18:53:50 -0300

    Added RAR extension support

--- src/scraper.js
+++ src/scraper.js
@@ -917,7 +917,8 @@ function isFileDownloadLink(string) {
     string.endsWith('.pdf')  || string.endsWith('.PDF')  ||
     string.endsWith('.doc')  || string.endsWith('.DOC')  ||
     string.endsWith('.docx') || string.endsWith('.DOCX') ||
-    string.endsWith('.zip')  || string.endsWith('.ZIP')
+    string.endsWith('.zip')  || string.endsWith('.ZIP')  ||
+    string.endsWith('.rar')  || string.endsWith('.RAR'))
   );
 }
 
